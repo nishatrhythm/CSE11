ea1c53b8b2fe2b38a38d7e4b02439c284fa132d8
nishatrhythm <66105148+nishatrhythm@users.noreply.github.com>
2023-09-04 13:26:32 +0600

    Initial commit

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
 // script.js
 
-// Function to add the "small" class to the header on scroll
-window.addEventListener('scroll', function () {
-    const header = document.querySelector('.navbar');
-    if (window.scrollY > 50) {
-        header.classList.add('small');
-    } else {
-        header.classList.remove('small');
-    }
-});
+// Function to handle any JavaScript functionality (if needed in the future)
+// ...
+// Add any other JavaScript functionality you may need here
+// ...
