74d0a79a0b08d51770c054bafaa113d2a6992985
nishatrhythm <66105148+nishatrhythm@users.noreply.github.com>
2023-09-08 12:57:21 +0600

    Data table and form updates

--- admin/script.js
+++ admin/script.js
@@ -1,66 +1,87 @@
-// Add this code to initialize DataTables
+// Add this code to initialize DataTables with column width adjustments
 $(document).ready(function () {
     $('#studentTable').DataTable({
-        paging: true, // Enable pagination
-        searching: true, // Enable search
-        ordering: true, // Enable sorting
-        order: [], // Default sorting (remove this line if you want DataTables to automatically sort)
-        responsive: true, // Enable responsiveness
+        paging: true,
+        searching: true,
+        ordering: true,
+        order: [],
+        responsive: true,
         language: {
             paginate: {
                 previous: '<i class="fas fa-chevron-left"></i>',
                 next: '<i class="fas fa-chevron-right"></i>'
             }
-        }
+        },
+        columnDefs: [
+            { width: '20%', targets: 0 }, // Adjust width of the "Student ID" column
+            { width: '50%', targets: 1 }, // Adjust width of the "Name" column
+            { width: '30%', targets: 2 }, // Adjust width of the "Actions" column
+        ]
     });
 });
 
 // Counter for generating unique IDs
 let idCounter = 0;
 
-// Function to generate a unique student ID
 function generateStudentId() {
     idCounter++;
-    // You can customize the format of the ID here
-    // This example uses "B1903050" followed by a unique number
-    return `B1903050${idCounter.toString().padStart(3, '0')}`;
+    const uniqueNumber = idCounter.toString().padStart(3, '0');
+    return `B1903050${uniqueNumber}`;
 }
 
 // Function to fetch and display student data
 function fetchAndDisplayStudents() {
+    // Check if the DataTable is already initialized
+    if ($.fn.DataTable.isDataTable("#studentTable")) {
+        // If initialized, clear and destroy the existing DataTable
+        $("#studentTable").DataTable().clear().destroy();
+    }
+
     fetch("/students.json")
         .then((response) => response.json())
         .then((data) => {
             // Data is an array of student objects
             const students = data;
 
-            // Access your table and tbody elements
-            const table = document.getElementById("studentTable");
-            const tbody = table.getElementsByTagName("tbody")[0];
+            // Access your table
+            const table = $("#studentTable").DataTable({
+                paging: true, // Enable pagination
+                searching: true, // Enable search
+                ordering: true, // Enable sorting
+                order: [], // Default sorting (remove this line if you want DataTables to automatically sort)
+                responsive: true, // Enable responsiveness
+                language: {
+                    paginate: {
+                        previous: '<i class="fas fa-chevron-left"></i>',
+                        next: '<i class="fas fa-chevron-right"></i>'
+                    }
+                }
+            });
 
             // Loop through the student data and create rows
             students.forEach((student) => {
-                const row = tbody.insertRow();
-
                 // Generate a unique ID for the student
-                const studentId = generateStudentId();
+                const studentId = `B1903050${student.id}`;
 
                 // Insert cell for ID
-                const idCell = row.insertCell(0);
-                idCell.textContent = studentId;
+                const idCell = studentId;
 
                 // Insert cell for Name
-                const nameCell = row.insertCell(1);
-                nameCell.textContent = student.name;
+                const nameCell = student.name;
 
                 // Insert cell for Actions (View, Edit, Delete buttons)
-                const actionsCell = row.insertCell(2);
-                actionsCell.innerHTML = `
+                const actionsCell = `
                     <button class="btn btn-primary btn-sm" onclick="viewStudent('${studentId}')">View</button>
                     <button class="btn btn-warning btn-sm" onclick="editStudent('${studentId}')">Edit</button>
                     <button class="btn btn-danger btn-sm" onclick="deleteStudent('${studentId}')">Delete</button>
                 `;
+
+                // Add a new row to the DataTable
+                table.row.add([idCell, nameCell, actionsCell]);
             });
+
+            // Draw the table to display the data
+            table.draw();
         })
         .catch((error) => {
             console.error("Error fetching data: ", error);
@@ -68,7 +89,20 @@ function fetchAndDisplayStudents() {
 }
 
 // Call the fetchAndDisplayStudents function when the page loads
-window.addEventListener("load", fetchAndDisplayStudents);
+$(document).ready(fetchAndDisplayStudents);
+
+// You can define your viewStudent, editStudent, and deleteStudent functions here
+function viewStudent(studentId) {
+    // Implement your logic for viewing a student's details
+}
+
+function editStudent(studentId) {
+    // Implement your logic for editing a student's details
+}
+
+function deleteStudent(studentId) {
+    // Implement your logic for deleting a student's record
+}
 
 // You can define your viewStudent, editStudent, and deleteStudent functions here
 function viewStudent(studentId) {
